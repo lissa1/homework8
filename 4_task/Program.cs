7e729b86c1ca316de84900bc11e6a4a722a92870
lissa <e-namestnikova@inbox.ru>
2022-07-22 14:40:57 +0700

    add forth task

---
+++ 4_task/Program.cs
@@ -0,0 +1,27 @@
+﻿// Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+int m = InputInt("введите число строк ");
+int n = InputInt("введите число столбцов ");
+int p = InputInt("ввeдите число ");
+int[,,] numbers = new int[m, n, p];
+FillandPrintArray(numbers);
+
+void FillandPrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = new Random().Next(10, 20);
+                Console.Write($"{array[i, j, k]} ({i}, {j}, {k}) ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
